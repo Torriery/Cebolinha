1e7faa70825f8374ba0978b06e1d1d152c286e56
torriery <andreastorrieri3@gmail.com>
2022-03-25 21:17:26 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿ string fraseDigitada, fraseComDislalia;
+Console.Write("digite uma frase com R:");
+fraseDigitada=Console.ReadLine()!;
+fraseComDislalia = fraseDigitada!
+.Replace("r","l")
+.Replace("R","L");
+Console.WriteLine(fraseComDislalia);
